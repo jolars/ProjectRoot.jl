90dac5cdd87f90f38281651e84a132c469d947d6
Johan Larsson <johanlarsson@outlook.com>
2024-01-20 15:15:39 +0100

    ci: change secrets name

--- src/Here.jl
+++ src/Here.jl
@@ -1,7 +1,18 @@
+[insert]
+
 module Here
 
 using FilePathsBase
 
+"""
+    iam()
+
+This function returns the current directory path. It checks for the presence of certain files or directories 
+(".here", "Project.toml", "Manifest.toml", ".git", ".svn") in the current directory and its parent directories. 
+If any of these files or directories are found, the function returns the path of that directory. 
+If none of these files or directories are found after traversing up to the root directory, 
+the function throws an error "Project root not found".
+"""
 function iam()
   current_dir = @__DIR__
 
@@ -28,9 +39,15 @@ function iam()
   error("Project root not found")
 end
 
-# Construct paths relative to the project root
+"""
+    here(args...)
+
+This function constructs paths relative to the project root. It takes any number of arguments, 
+which represent the path components, and joins them with the path returned by the `iam()` function.
+"""
 here(args...) = joinpath(iam(), args...)
 
 export here
 
 end
+```
