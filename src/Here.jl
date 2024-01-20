23761d66e00d0cc0f38567762daebf04c83a74fd
Johan Larsson <johanlarsson@outlook.com>
2024-01-20 14:41:26 +0100

    feat: add here() and iam() functions

--- src/Here.jl
+++ src/Here.jl
@@ -1,5 +1,36 @@
 module Here
 
-# Write your package code here.
+using FilePathsBase
+
+function iam()
+  current_dir = @__DIR__
+
+  patterns = [
+    Dict(".here" => :file),
+    Dict("Project.toml" => :file),
+    Dict("Manifest.toml" => :file),
+    Dict(".git" => :dir),
+    Dict(".svn" => :dir),
+  ]
+
+  # Recurse up the directory tree until we find a match
+  while current_dir != "/"
+    for pattern_dict in patterns
+      for (pattern, type) in pattern_dict
+        if type == :file && isfile(joinpath(current_dir, pattern)) ||
+           type == :dir && isdir(joinpath(current_dir, pattern))
+          return current_dir
+        end
+      end
+    end
+    current_dir = dirname(current_dir)
+  end
+  error("Project root not found")
+end
+
+# Construct paths relative to the project root
+here(args...) = joinpath(iam(), args...)
+
+export here
 
 end
