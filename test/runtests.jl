86d192a3a0e3f0ef305df80e669a73762e33c378
Johan Larsson <johanlarsson@outlook.com>
2024-01-20 14:40:51 +0100

    tests: add a simple test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using Here
 using Test
 
 @testset "Here.jl" begin
-    # Write your tests here.
+  mktempdir() do dir
+    # Create a .here file in the temporary directory
+    touch(joinpath(dir, ".here"))
+    # Change the working directory to the temporary directory
+    cd(dir) do
+      # Test the iam function
+      @test Here.iam() == dir
+    end
+  end
 end
