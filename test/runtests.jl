1d54eb872dcdc39ef24ae6b6608236bf1b1c8940
Johan Larsson <johanlarsson@outlook.com>
2024-01-21 20:08:00 +0100

    tests: fix failing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,17 @@ using Test
   mktempdir() do dir
     # Create a .here file in the temporary directory
     touch(joinpath(dir, ".here"))
-    # Change the working directory to the temporary directory
-    cd(dir) do
-      # Test the iam function
-      @test Here.iam() == dir
-    end
+
+    script_file = joinpath(dir, "script.jl")
+    touch(script_file)
+
+    file = open(script_file, "w")
+    println(file, "using Here;print(@here())")
+    close(file)
+
+    out = read(`julia $script_file`, String)
+    print(out)
+
+    @test out == dir
   end
 end
