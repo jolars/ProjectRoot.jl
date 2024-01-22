d8c486c5355402d422a9ca071f157bda4d9b1125
Johan Larsson <johanlarsson@outlook.com>
2024-01-22 08:22:16 +0100

    fix: always return at least file system root

--- src/Here.jl
+++ src/Here.jl
@@ -7,7 +7,7 @@ module Here
 
 using FilePathsBase
 
-function find_root_dir(current_dir)
+function find_root(dir)
   patterns = [
     Dict(".here" => :file),
     Dict("Project.toml" => :file),
@@ -16,19 +16,29 @@ function find_root_dir(current_dir)
     Dict(".svn" => :dir)
   ]
 
-  # Recurse up the directory tree until we find a match
-  while current_dir != "/"
+  # Recursive search for root directory.
+  while true
+    prev_dir = dir
+
     for pattern_dict in patterns
       for (pattern, type) in pattern_dict
-        if type == :file && isfile(joinpath(current_dir, pattern)) ||
-           type == :dir && isdir(joinpath(current_dir, pattern))
-          return current_dir
+        if type == :file && isfile(joinpath(dir, pattern)) ||
+           type == :dir && isdir(joinpath(dir, pattern))
+          return dir
         end
       end
     end
-    current_dir = dirname(current_dir)
+
+    # Go to parent directory
+    dir = dirname(dir)
+
+    if dir == prev_dir
+      # Cannot go up any further; must be file system root.
+      return dir
+    end
   end
-  error("Project root not found")
+
+  @assert false "We should not have ended up here."
 end
 
 """
@@ -37,11 +47,15 @@ end
 This macro returns a path relative to the root directory of the project.
 """
 macro here(args::String...)
-  current_dir = String(__source__.file)
-  if startswith(current_dir, "REPL")
-    current_dir = pwd()
+  source_file = String(__source__.file)
+
+  if startswith(source_file, "REPL")
+    return pwd()
   end
-  joinpath(find_root_dir(expanduser(current_dir)), args...)
+
+  current_dir = dirname(source_file)
+
+  return joinpath(find_root(current_dir), args...)
 end
 
 export @here
